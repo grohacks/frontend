d9c4ad4e9538c8a8fec17e612b68672e33903697
Nishant <nishantrao.2005@gmail.com>
2024-11-12 21:31:32 +0530

    Add start script to package.json

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" style={{ flexGrow: 1 }}>
-                        School Management
+                        CRUD Management
                     </Typography>
                     <Button color="inherit" component={Link} to="/students">Students</Button>
                     <Button color="inherit" component={Link} to="/faculty">Faculty</Button>
